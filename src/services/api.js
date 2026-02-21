6aa79bdd259eaba556a7446b84daf32d59145fdd
MarkClifford02 <markcliffordmangoang.com>
2026-02-22 00:45:15 +0800

    Final Fix: Connect to Render backend URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // Dinagdagan natin ng /api sa dulo para mag-match sa server.js routes
+  // Siguraduhin na may /api sa dulo
   baseURL: "https://lab-5-vue-frontend-ai-ui-integration.onrender.com/api" 
 });
