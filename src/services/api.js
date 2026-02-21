b534c73da33944d2cc638d01c3aa51009e2e2ed7
MarkClifford02 <markcliffordmangoang.com>
2026-02-21 22:39:33 +0800

    Fix: Added /api to baseURL in api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
-// src/services/api.js
 import axios from "axios";
 
 export default axios.create({
-  // Eto dapat ang gamitin mong URL dahil ito ang "Connected" sa database
-  baseURL: "https://lab-5-vue-frontend-ai-ui-integration.onrender.com" 
+  // Dinagdagan natin ng /api sa dulo para mag-match sa server.js routes
+  baseURL: "https://lab-5-vue-frontend-ai-ui-integration.onrender.com/api" 
 });
