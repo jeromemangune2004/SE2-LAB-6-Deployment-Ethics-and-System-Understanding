335dacfd058def8f0720b58d650e2dd2c5e0ca7d
MarkClifford02 <markcliffordmangoang.com>
2026-02-26 23:00:03 +0800

    fix: updated base url to railway backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // Dapat itong tumuro sa iyong BACKEND service, hindi sa frontend:
-  baseURL: "https://se2-lab-6-deployment-ethics-and-system.onrender.com/api" 
+  baseURL: "https://se2-lab-6-deployment-ethics-and-system-understan-production.up.railway.app/api" 
 });
