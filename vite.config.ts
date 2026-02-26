14387ed014e053f19f74f5f321d3c4981fb53b02
MarkClifford02 <markcliffordmangoang.com>
2026-02-26 22:30:05 +0800

    fix: clean vite config and set base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Lab-5-Vue-Frontend-AI-UI-Integration/', // <- ADD THIS for GitHub Pages
+  base: '/',
   plugins: [
     vue(),
     vueDevTools(),
